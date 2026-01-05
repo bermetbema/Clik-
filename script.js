4e7404e1882c7a096e06c7fbdef19d065ae091b0
bermetbema <bermetozgorushova02@gmail.com>
2026-01-05 10:17:43 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// let img = document.getElementById('photo')
+
+//  console.log( img.hasAttribute('scr'))
+//  img.setAttribute(scr" ",'')
+ 
+const Clik = ()=>{
+    const img =
+    document.getElementById('photo');
+    img.setAttribute('src','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUAX23TLmLUDFMRc-yPictpjVntgpNb0cI2A&s')
+
+}
